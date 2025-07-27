07d2a67d37767556ff877bcdcec1fc825669cec6
troopdegen <mel@innvertir.com>
2025-07-26 18:54:40 -0600

    add Funnel Display and Ledger fonts with correct paths

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,58 +1,42 @@
-// import localFont from 'next/font/local'
-import { Raleway, Space_Grotesk as SpaceGrotesk, Inter } from 'next/font/google'
+import localFont from "next/font/local";
+import { Raleway, Space_Grotesk as SpaceGrotesk } from "next/font/google";
 
-// Fallback to Inter for Funnel Display until font files are available
-export const funnelDisplay = Inter({
-  subsets: ['latin'],
-  variable: '--font-funnel',
-  weight: ['400', '500', '600'],
-  display: 'swap',
-})
+// Define your custom font
+export const funnelDisplay = localFont({
+  src: [
+    {
+      path: "../public/fonts/FunnelDisplay-VariableFont_wght.ttf",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  variable: "--font-funnel",
+  display: "swap",
+  preload: true,
+});
 
-// Fallback to Inter for Ledger until font files are available  
-export const ledger = Inter({
-  subsets: ['latin'],
-  variable: '--font-ledger',
-  weight: ['400'],
-  display: 'swap',
-})
-
-// TODO: Uncomment when font files are available
-// export const funnelDisplay = localFont({
-//   src: [
-//     {
-//       path: '../../public/fonts/FunnelDisplay-VariableFont_wght.ttf',
-//       weight: '400',
-//       style: 'normal',
-//     },
-//   ],
-//   variable: '--font-funnel',
-//   display: 'swap',
-//   preload: true,
-// })
-
-// export const ledger = localFont({
-//   src: [
-//     {
-//       path: '../../public/fonts/Ledger-Regular.ttf',
-//       weight: '400',
-//       style: 'normal',
-//     },
-//   ],
-//   variable: '--font-ledger',
-//   display: 'swap',
-//   preload: true,
-// })
+export const ledger = localFont({
+  src: [
+    {
+      path: "../public/fonts/Ledger-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  variable: "--font-ledger",
+  display: "swap",
+  preload: true,
+});
 
 const raleway = Raleway({
-  subsets: ['latin'],
-  variable: '--raleway',
-})
+  subsets: ["latin"],
+  variable: "--raleway",
+});
 
 const spaceGrotesk = SpaceGrotesk({
-  subsets: ['latin'],
-  variable: '--space-grotesk',
-})
+  subsets: ["latin"],
+  variable: "--space-grotesk",
+});
 
 // Export all fonts
 export const fonts = {
@@ -60,4 +44,4 @@ export const fonts = {
   ledger,
   raleway,
   spaceGrotesk,
-}
+};
